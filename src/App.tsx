7c20541960e0c5709a9abbf8cfe03af0f1912a8b
Brandon-Lee Schultz <schultzblee@gmail.com>
2025-09-02 21:56:35 +0200

    update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import {
-  CookieBanner,
   DisclaimerPage,
   PrivacyPolicyPage,
   ReturnsDeliveryPage,
@@ -27,7 +26,6 @@ const App = () => (
           <Route path="terms-and-conditions" element={<TermsAndConditionsPage />} />
           <Route path="returns-delivery" element={<ReturnsDeliveryPage />} />
           <Route path="disclaimer" element={<DisclaimerPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
